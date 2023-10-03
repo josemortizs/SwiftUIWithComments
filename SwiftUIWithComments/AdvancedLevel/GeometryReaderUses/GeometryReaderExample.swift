031e0622681fa8cbf01ee681200eb6bf1f117688
José Manuel Ortiz Sánchez <josemortizs@gmail.com>
2023-10-03 20:23:49 +0200

    GeometryReader example and util extension view

---
+++ SwiftUIWithComments/AdvancedLevel/GeometryReaderUses/GeometryReaderExample.swift
@@ -0,0 +1,44 @@
+//
+//  GeometryReaderExample.swift
+//  SwiftUIWithComments
+//
+//  Created by Jose Manuel Ortiz Sanchez on 3/10/23.
+//
+// origin: https://www.fivestars.blog/articles/swiftui-share-layout-information/
+
+import SwiftUI
+
+struct GeometryReaderExample: View {
+    var body: some View {
+        Text("Hello, World!")
+            .readSize { size in
+                print("The size of this Text is: \(size)")
+            }
+    }
+}
+
+#Preview {
+    GeometryReaderExample()
+}
+
+/*  --------------------------------------------------------------
+    With this view extension we can get the size of any child view
+    by using a simple view modifier.
+    --------------------------------------------------------------
+ */
+extension View {
+  func readSize(onChange: @escaping (CGSize) -> Void) -> some View {
+    background(
+      GeometryReader { geometryProxy in
+        Color.clear
+          .preference(key: SizePreferenceKey.self, value: geometryProxy.size)
+      }
+    )
+    .onPreferenceChange(SizePreferenceKey.self, perform: onChange)
+  }
+}
+
+struct SizePreferenceKey: PreferenceKey {
+  static var defaultValue: CGSize = .zero
+  static func reduce(value: inout CGSize, nextValue: () -> CGSize) {}
+}
