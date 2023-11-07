bbdab9b50668c288160787589444095d2761f4cf
José Manuel Ortiz Sánchez <josemortizs@gmail.com>
2023-11-07 20:40:31 +0100

    Update erroralertexample, not finished

--- SwiftUIWithComments/AdvancedLevel/CustomBindings/ErrorAlertExample.swift
+++ SwiftUIWithComments/AdvancedLevel/CustomBindings/ErrorAlertExample.swift
@@ -4,12 +4,15 @@
 //
 //  Created by Jose Manuel Ortiz Sanchez on 6/11/23.
 //
+//  Origin: https://youtu.be/lncOFL3Qsns?si=UnHLMba6IrSThCAi
+//  https://youtu.be/lncOFL3Qsns?si=eRjKENNwuwG7CjQG&t=1556
 
 import SwiftUI
 
 struct ErrorAlertExample: View {
     
-    @State private var error: Error? = nil
+    // @State private var error: Error? = nil
+    @State private var alert: MyCustomAlert? = nil
     
     var body: some View {
         Button {
@@ -17,17 +20,40 @@ struct ErrorAlertExample: View {
         } label: {
             Text("Click me")
         }
-        .alert(error?.localizedDescription ?? "Error", isPresented: Binding(value: $error)) {
-            Button("OK") {
-                
+        .alert(alert?.title ?? "Error", isPresented: Binding(value: $alert), actions: {
+            alert?.getButtonsForAlert(onOkPressed: {
+                // Logic onKeyPressed
+            })
+        }, message: {
+            if let subTitle = alert?.subTitle {
+                Text(subTitle)
             }
-        }
+        })
     }
     
-    enum MyCustomError: Error, LocalizedError {
+    // enum MyCustomError: Error, LocalizedError {
+        
+    //     case noInternetConnection
+    //     case dataNotFound
+    //     case urlError(error: Error)
+        
+    //     var errorDescription: String? {
+    //         switch self {
+    //         case .noInternetConnection:
+    //             return "Please check your internet connection and try again."
+    //         case .dataNotFound:
+    //             return "There was an error loading data. Please try again!"
+    //         case .urlError(let error):
+    //             return "Error: \(error.localizedDescription)"
+    //         }
+    //     }
+    // }
+    
+    enum MyCustomAlert: Error, LocalizedError {
         
         case noInternetConnection
         case dataNotFound
+        case urlError(error: Error)
         
         var errorDescription: String? {
             switch self {
@@ -35,6 +61,44 @@ struct ErrorAlertExample: View {
                 return "Please check your internet connection and try again."
             case .dataNotFound:
                 return "There was an error loading data. Please try again!"
+            case .urlError(let error):
+                return "Error: \(error.localizedDescription)"
+            }
+        }
+
+        var title: String {
+            switch self {
+                case .noInternetConnection: return "No internet connection"
+                case .dataNotFound: return "No data"
+                case .urlError: return "URLError"
+            }
+        }
+
+        var subTitle: String? {
+            switch self {
+                case .noInternetConnection: return "Please check your internet connection and try again."
+                case .dataNotFound: return nil
+                case .urlError(let error): return "Error: \(error.localizedDescription)"
+            }
+        }
+        
+        @ViewBuilder
+        func getButtonsForAlert(
+            onOkPressed: @escaping () -> Void
+        ) -> some View {
+            switch self {
+            case .noInternetConnection:
+                Button("Ok") {
+                    onOkPressed()
+                }
+            case .dataNotFound:
+                Button("Retry") {
+                    
+                }
+            default:
+                Button("Delete", role: .destructive) {
+                    
+                }
             }
         }
     }
@@ -45,8 +109,8 @@ struct ErrorAlertExample: View {
         if isSuccessful {
             // do something
         } else {
-            let myError: Error = MyCustomError.noInternetConnection
-            error = myError
+            let alert: MyCustomAlert = .dataNotFound
+            self.alert = alert
         }
     }
 }
