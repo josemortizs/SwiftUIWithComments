f10dcc969bfe7c9a9b7e1fba1f8f2d1b225b7659
José Manuel Ortiz Sánchez <josemortizs@gmail.com>
2023-11-08 20:27:46 +0100

    ErrorAlertExample with protocol and view extension

--- SwiftUIWithComments/AdvancedLevel/CustomBindings/ErrorAlertExample.swift
+++ SwiftUIWithComments/AdvancedLevel/CustomBindings/ErrorAlertExample.swift
@@ -5,53 +5,68 @@
 //  Created by Jose Manuel Ortiz Sanchez on 6/11/23.
 //
 //  Origin: https://youtu.be/lncOFL3Qsns?si=UnHLMba6IrSThCAi
-//  https://youtu.be/lncOFL3Qsns?si=eRjKENNwuwG7CjQG&t=1556
 
 import SwiftUI
 
+/*
+    We declare the protocol that all data models that want to
+    provide information to a possible alert must comply with.
+ */
+protocol AppAlert {
+    var title: String { get }
+    var subtitle: String? { get }
+    var buttons: AnyView { get }
+}
+
+/*
+    We define a View extension so we can call our function from
+    any view.
+ */
+extension View {
+    
+    func showCustomAlert<T: AppAlert>(alert: Binding<T?>) -> some View {
+        self
+            .alert(alert.wrappedValue?.title ?? "Error", isPresented: Binding(value: alert), actions: {
+                alert.wrappedValue?.buttons
+            }, message: {
+                if let subTitle = alert.wrappedValue?.subtitle {
+                    Text(subTitle)
+                }
+            })
+    }
+}
+
 struct ErrorAlertExample: View {
     
-    // @State private var error: Error? = nil
     @State private var alert: MyCustomAlert? = nil
     
     var body: some View {
         Button {
             saveData()
         } label: {
-            Text("Click me")
+            Text("Show alert")
         }
-        .alert(alert?.title ?? "Error", isPresented: Binding(value: $alert), actions: {
-            alert?.getButtonsForAlert(onOkPressed: {
-                // Logic onKeyPressed
-            })
-        }, message: {
-            if let subTitle = alert?.subTitle {
-                Text(subTitle)
-            }
-        })
+        .showCustomAlert(alert: $alert)
+        /*
+         1. We can use showCustomAlert because it is inside ErrorAlertExample,
+            which is of type View.
+         2. We can pass, as an argument to the show Custom Alert function,
+            our alert, of type MyCustomAlert, because it implements the
+            AppAlert protocol.
+         */
     }
     
-    // enum MyCustomError: Error, LocalizedError {
+    /*
+        The LocalizedError protocol allows us to implement errorDescription,
+        since MyCustomAlert is an Error implementation, making it appropriate
+        to use the error.errorDescription convention
+     */
+    enum MyCustomAlert: Error, LocalizedError, AppAlert {
         
-    //     case noInternetConnection
-    //     case dataNotFound
-    //     case urlError(error: Error)
-        
-    //     var errorDescription: String? {
-    //         switch self {
-    //         case .noInternetConnection:
-    //             return "Please check your internet connection and try again."
-    //         case .dataNotFound:
-    //             return "There was an error loading data. Please try again!"
-    //         case .urlError(let error):
-    //             return "Error: \(error.localizedDescription)"
-    //         }
-    //     }
-    // }
-    
-    enum MyCustomAlert: Error, LocalizedError {
-        
-        case noInternetConnection
+        case noInternetConnection(
+            onOKPressed: () -> Void,
+            onRetryPressed: () -> Void
+        )
         case dataNotFound
         case urlError(error: Error)
         
@@ -74,7 +89,7 @@ struct ErrorAlertExample: View {
             }
         }
 
-        var subTitle: String? {
+        var subtitle: String? {
             switch self {
                 case .noInternetConnection: return "Please check your internet connection and try again."
                 case .dataNotFound: return nil
@@ -82,22 +97,26 @@ struct ErrorAlertExample: View {
             }
         }
         
-        @ViewBuilder
-        func getButtonsForAlert(
-            onOkPressed: @escaping () -> Void
-        ) -> some View {
+        var buttons: AnyView {
+            AnyView(getButtonsForAlert)
+        }
+        
+        @ViewBuilder var getButtonsForAlert: some View {
             switch self {
-            case .noInternetConnection:
+            case .noInternetConnection(let onOkPressed, let onRetryPressed):
                 Button("Ok") {
                     onOkPressed()
                 }
+                Button("Retry") {
+                    onRetryPressed()
+                }
             case .dataNotFound:
                 Button("Retry") {
-                    
+                    // Do something here, don't be lazy...
                 }
             default:
                 Button("Delete", role: .destructive) {
-                    
+                    // Do something here, don't be lazy...
                 }
             }
         }
@@ -107,10 +126,13 @@ struct ErrorAlertExample: View {
         let isSuccessful: Bool = false
         
         if isSuccessful {
-            // do something
+            // // Do something here, don't be lazy...
         } else {
-            let alert: MyCustomAlert = .dataNotFound
-            self.alert = alert
+            alert = .noInternetConnection(onOKPressed: {
+                print("OK pressed")
+            }, onRetryPressed: {
+                print("Retry pressed")
+            })
         }
     }
 }
